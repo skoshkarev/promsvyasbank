99d4ffa97583198ae8284466078d8d777e9d1498
Sergey Koshkarev <sergey.koshkarev@yandex.ru>
2013-09-09 00:26:08 +0700

    Create psb_config.php

---
+++ psb_config.php
@@ -0,0 +1,14 @@
+<?php
+    define('PSB_TERMINAL', 00000000);
+    define('PSB_MERCHANT', 00000000000000);
+    define('PSB_MERCHANT_NAME', 'TEST_MERCH');
+    define('PSB_MERCHANT_EMAIL', 'youremail@gmail.com');
+    define('PSB_KEY', 'C50E43460306E0F9D6D59F1AAA925C45');
+    define('PSB_BACK_REF', 'http://www.yoursite.com/backref');
+    define('PSB_GATEWAY_URL_SANDBOX', 'http://193.200.10.117:8080/cgi-bin/cgi_link');
+    define('PSB_GATEWAY_URL_LIVE', 'https://3ds.payment.ru/cgi-bin/cgi_link');
+
+    define('PSB_SANDBOX', true);
+    define('PSB_LOG_ENABLED', true);
+    define('PSB_LOG_PATH', 'logs'); // Директория относительно расположения самого класса
+  
